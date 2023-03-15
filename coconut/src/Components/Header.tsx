896aec378624531cbadb4b062bdf6d51c6ae92c8
HamsterStudent <hwanyamyu@gmail.com>
2023-03-15 20:13:19 +0900

    header

---
+++ coconut/src/Components/Header.tsx
@@ -0,0 +1,41 @@
+import styled from "styled-components";
+
+const Nav = styled.div`
+  width: 100%;
+  height: 20px;
+  background-color: plum;
+  display: flex;
+  justify-content: space-between;
+  border-bottom: solid 0.7px;
+`;
+
+const Dropdown = styled.div``;
+
+const Right = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
+
+const Item = styled.div`
+  padding: 0 10px;
+  border: dotted 0.7px;
+`;
+
+function Header() {
+  return (
+    <Nav>
+      <Dropdown>
+        Logo
+        <Item>Theme</Item>
+        <Item>Contact</Item>
+      </Dropdown>
+      <Right>
+        <Item>fullscreen</Item>
+        <Item>clock</Item>
+        <Item>date</Item>
+      </Right>
+    </Nav>
+  );
+}
+
+export default Header;
