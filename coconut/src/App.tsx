a25a73ad06474eff1e38b8381f14c4c526c9e9e0
HamsterStudent <hwanyamyu@gmail.com>
2023-05-02 23:57:14 +0900

    :construction: drag & drop
    - page/info의 내용을 app으로 옯겨서 작업
    - app에서 모달 여러개를 뿌려서 각각 페이지를 넣는 방식으로 작업 예정
    - 모달 각각의 초기 포지션 지정할 수 있게 작업 예정

--- coconut/src/App.tsx
+++ coconut/src/App.tsx
@@ -1,12 +1,92 @@
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 import Header from "./Components/Header";
-import Info from "./Pages/Info";
+import styled from "styled-components";
+// import Info from "./Pages/Info";
+
+const Info = styled.div`
+  width: 30vw;
+  height: 30vh;
+  background-color: plum;
+  position: absolute;
+`;
+
+const ContainerWrap = styled.div`
+  width: 100%;
+  background-color: tomato;
+`;
 
 function App() {
+  const container = useRef<HTMLDivElement>(null); // 드래그 할 영역 네모 박스 Ref
+  const dragComponent = useRef<HTMLDivElement>(null); // // 움직일 드래그 박스 Ref
+  const [initialPos, setInitialPos] = useState({ x: 0, y: 0 }); // 드래그 전 포지션값 (e.target.offset의 상대 위치)
+  const [clientPos, setClientPos] = useState({ x: 0, y: 0 }); // 실시간 커서위치인 e.client를 갱신하는값
+  const [pos, setPos] = useState({ left: 0, top: 0 }); // 실제 drag할 요소가 위치하는 포지션값
+
+  const onDragStart = (e: React.DragEvent<HTMLDivElement>) => {
+    // 고스트이미지 제거
+    // var img = new Image();
+    // img.src =
+    //   "data:image/gif;base64,R0lGODlhAQABAIAAAAUEBAAAACwAAAAAAQABAAACAkQBADs=";
+    // e.dataTransfer.setDragImage(img, 0, 0);
+
+    const temp = { ...initialPos };
+    temp["x"] = e.currentTarget.offsetLeft;
+    temp["y"] = e.currentTarget.offsetTop;
+    console.log("initialPosTemp", temp);
+    setInitialPos(temp);
+
+    const clientTemp = { ...clientPos };
+    clientTemp["x"] = e.clientX;
+    clientTemp["y"] = e.clientY;
+    setClientPos(clientTemp);
+  };
+  const onDrag = (e: React.DragEvent<HTMLDivElement>) => {
+    const posTemp = { ...pos };
+    posTemp["left"] = e.currentTarget.offsetLeft + e.clientX - clientPos.x;
+    posTemp["top"] = e.currentTarget.offsetTop + e.clientY - clientPos.y;
+
+    setPos(posTemp);
+
+    const clientPosTemp = { ...clientPos };
+    clientPosTemp["x"] = e.clientX;
+    clientPosTemp["y"] = e.clientY;
+    setClientPos(clientPosTemp);
+
+    console.log(container.current?.clientWidth);
+  };
+  const onDragOver = (e: React.DragEvent<HTMLDivElement>) => {
+    e.preventDefault();
+  };
+  const isInsideDragArea = (e: any) => {
+    if (e.clientX < 0 || e.clientY < 0) {
+      return false;
+    } else {
+      return true;
+    }
+  };
+  const onDragEnd = (e: React.DragEvent<HTMLDivElement>) => {
+    // if (!isInsideDragArea(e)) {
+    //   const posTemp = { ...pos };
+    //   posTemp["left"] = initialPos.x;
+    //   posTemp["top"] = initialPos.y;
+    //   setPos(posTemp);
+    // }
+  };
+
   return (
     <div className="App">
-      <Header></Header>
-      <Info />
+      <Header />
+      <ContainerWrap ref={container}>
+        <Info
+          ref={dragComponent}
+          draggable
+          onDragStart={(e) => onDragStart(e)}
+          onDrag={(e) => onDrag(e)}
+          onDragOver={(e) => onDragOver(e)}
+          onDragEnd={(e) => onDragEnd(e)}
+          style={{ left: pos.left, top: pos.top }}
+        />
+      </ContainerWrap>
     </div>
   );
 }
--- coconut/src/Pages/Info.tsx
+++ coconut/src/Pages/Info.tsx
@@ -1,64 +1,8 @@
-import { useRef } from "react";
+import { useRef, useState } from "react";
 import styled from "styled-components";
 
-const InfoWrap = styled.div`
-  width: 50vw;
-  height: 50vh;
-  background: plum;
-  cursor: pointer;
-`;
-
-interface IPosition {
-  x: number;
-  y: number;
-  e: React.DragEvent<HTMLDivElement>;
-}
-
-function setTranslate({ x, y, e }: IPosition) {
-  e.currentTarget.style.transform = "translate3d(" + x + "px, " + y + "px, 0)";
-}
-
 function Info() {
-  let initialX;
-  let initialY;
-  let currentX;
-  let currentY;
-
-  const onDragStart = (e: React.DragEvent<HTMLDivElement>) => {
-    e.dataTransfer.effectAllowed = "move";
-    console.log("X: " + e.clientX + " | Y: " + e.clientY);
-    console.log(e.currentTarget);
-    // e.preventDefault();
-    initialX = e.clientX;
-    initialY = e.clientY;
-  };
-  const onDrag = (e: React.DragEvent<HTMLDivElement>) => {
-    // e.preventDefault();
-    if (e.type == "drag") {
-      console.log(e);
-      e.currentTarget.style.left = `${e.clientX}px`;
-      e.currentTarget.style.top = `${e.clientY}px`;
-      initialX = e.clientX;
-      initialY = e.clientY;
-
-      let x = e.clientX;
-      let y = e.clientY;
-      setTranslate({ x, y, e });
-    }
-  };
-  const onDragEnd = (e: React.DragEvent<HTMLDivElement>) => {
-    e.preventDefault();
-    console.log("X: " + e.clientX + " | Y: " + e.clientY);
-    console.log(e.currentTarget);
-  };
-  return (
-    <InfoWrap
-      draggable
-      onDragStart={onDragStart}
-      onDrag={onDrag}
-      // onDragEnd={onDragEnd}
-    ></InfoWrap>
-  );
+  return;
 }
 
 export default Info;
