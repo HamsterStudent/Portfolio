085939c731c7fdb5edc945ff0f69d47102c6c5dd
HamsterStudent <hwanyamyu@gmail.com>
2024-01-18 17:55:57 +0900

    :lipstick: Style : add routing animation

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -1,15 +1,42 @@
 import React from "react";
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, useLocation } from "react-router-dom";
 import Project from "../Pages/Project";
 import Main from "../Pages/Main/Main";
+import { useTransition, animated } from "@react-spring/web";
 
 export default function Router() {
+  const location = useLocation();
+  const transitions = useTransition(location, {
+    from: {
+      opacity: 0,
+      transform: "translate3d(100vw, 0, 0)",
+      transition: "ease-in-out",
+      height: "0",
+    },
+    enter: {
+      opacity: 1,
+      transform: "translate3d(0, 0, 0)",
+      transition: "ease-in-out",
+      height: "100%",
+    },
+    leave: {
+      opacity: 0,
+      transform: "translate3d(-120vw, 0, 0)",
+      transition: "ease-in-out",
+      height: "0",
+    },
+  });
+
   return (
     <>
-      <Routes>
-        <Route path="/" element={<Main />} />
-        <Route path="/project" element={<Project />} />
-      </Routes>
+      {transitions((style, item) => (
+        <animated.div key={item.key} style={style}>
+          <Routes location={item}>
+            <Route path="/" element={<Main />} />
+            <Route path="/project" element={<Project />} />
+          </Routes>
+        </animated.div>
+      ))}
     </>
   );
 }
