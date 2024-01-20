f5cd1c347a6fe62164bb6df4351dee4f6226562c
HamsterStudent <hwanyamyu@gmail.com>
2024-01-20 11:30:34 +0900

    :fire: Remove : remove useless code

--- src/App.tsx
+++ src/App.tsx
@@ -11,18 +11,6 @@ import {
   themeAtom,
   windowDisplayAtom,
 } from "./recoil/atom";
-import Resume from "./Pages/resume";
-import Blog from "./Pages/Blog";
-import Dungeon from "./Pages/Dungeon";
-import Project from "./Pages/Project";
-import Tools from "./Pages/Tools";
-import About from "./Pages/About";
-import Sticker from "./shared/components/Sticker";
-import ProdListPost from "./Pages/CodingPosts/ProdListPost";
-import SessionPost from "./Pages/CodingPosts/SessionPost";
-import ProxyPost from "./Pages/CodingPosts/ProxyPost";
-import MapRefact from "./Pages/CodingPosts/MapRefact";
-import MusicPlayer from "./Pages/musicPlayer/MusicPlayer";
 import Router from "./router/router";
 
 const marquee = keyframes`
