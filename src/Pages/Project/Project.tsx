f578200bb222884ded33576d8cf5c1df093f08da
HamsterStudent <hwanyamyu@gmail.com>
2024-01-26 10:38:38 +0900

    :lipstick: Style : add project mobile category

--- src/Pages/Project/Project.tsx
+++ src/Pages/Project/Project.tsx
@@ -8,12 +8,12 @@ import Overview from "./components/Overview";
 interface IDisplay {
   isDesktop: boolean;
 }
-/* height: ${(props) => (props.isDesktop ? "auto" : "580px")}; */
 const ContentsWrap = styled.section<IDisplay>`
   height: calc(100% - 20px);
   max-width: 1200px;
   padding: 15px;
   margin: 10px auto;
+  padding-top: ${(props) => (props.isDesktop ? "10px" : "30px")};
   font-size: 14px;
   border: solid 1px;
   display: flex;
@@ -25,6 +25,23 @@ const CategoryWrap = styled.section`
   flex-direction: column;
   justify-content: space-between;
   border-right: solid 1px;
+`;
+
+const MobileCategory = styled.section`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: auto;
+  border: solid 1px;
+  z-index: 10;
+  background-color: #ffffff92;
+  h2 {
+    width: 100%;
+    height: 100%;
+    text-align: center;
+    line-height: 30px;
+  }
 `;
 
 const blink = keyframes`
@@ -47,11 +64,23 @@ const ProjectList = styled.ul`
     animation: ${blink} 2.5s ease-in-out infinite;
   }
 `;
+const MobileProjectList = styled(ProjectList)`
+  width: 100%;
+  border-top: solid 1px;
+  li {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+    height: 30px;
+  }
+`;
 
 const SNS = styled.ul``;
 
 const Project = () => {
   const [tabName, setTabName] = useState("overview");
+  const [tabOpen, setTabOpen] = useState(false);
   const navigate = useNavigate();
 
   const handleTabClick = (tabName: string) => {
@@ -59,7 +88,7 @@ const Project = () => {
   };
 
   const isDesktop = useMediaQuery({
-    query: "(min-width : 700px) and (max-width :1920px)",
+    query: "(min-width : 600px) and (max-width :1920px)",
   });
 
   const tabList = [
@@ -77,38 +106,65 @@ const Project = () => {
   return (
     <>
       <ContentsWrap isDesktop={isDesktop}>
-        <CategoryWrap>
-          <div>
-            <h2>Project</h2>
-            <p
-              onClick={() => {
-                navigate(`/`);
-              }}
-            >
-              ← Back
-            </p>
-          </div>
+        {isDesktop ? (
+          <CategoryWrap>
+            <div>
+              <h2>Project</h2>
+              <p
+                onClick={() => {
+                  navigate(`/`);
+                }}
+              >
+                ← Back
+              </p>
+            </div>
 
-          <ProjectList>
-            {tabList.map((data) => {
-              return (
-                <li
-                  key={data}
-                  onClick={() => handleTabClick(data)}
-                  className={tabName === data ? "active" : undefined}
-                >
-                  <div>
-                    <p>{data}</p>
-                  </div>
-                </li>
-              );
-            })}
-          </ProjectList>
-          <SNS>
-            <li>Github</li>
-            <li>hwanyamyu@gmail.com</li>
-          </SNS>
-        </CategoryWrap>
+            <ProjectList>
+              {tabList.map((data) => {
+                return (
+                  <li
+                    key={data}
+                    onClick={() => handleTabClick(data)}
+                    className={tabName === data ? "active" : undefined}
+                  >
+                    <div>
+                      <p>{data}</p>
+                    </div>
+                  </li>
+                );
+              })}
+            </ProjectList>
+            <SNS>
+              <li>Github</li>
+              <li>hwanyamyu@gmail.com</li>
+            </SNS>
+          </CategoryWrap>
+        ) : (
+          <MobileCategory
+            onClick={() => {
+              setTabOpen((prev) => !prev);
+            }}
+          >
+            <h2>{tabName}</h2>
+            {tabOpen ? (
+              <MobileProjectList>
+                {tabList.map((data) => {
+                  return (
+                    <li
+                      key={data}
+                      onClick={() => handleTabClick(data)}
+                      className={tabName === data ? "active" : undefined}
+                    >
+                      <div>
+                        <p>{data}</p>
+                      </div>
+                    </li>
+                  );
+                })}
+              </MobileProjectList>
+            ) : null}
+          </MobileCategory>
+        )}
 
         {tabName === "overview" ? (
           <Overview tabList={tabList} handleTabClick={handleTabClick} />
